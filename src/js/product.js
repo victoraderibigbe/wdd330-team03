35856a8adad802187387d1221ed4edc2e39fa5fe
Artur Calderon <artur.calderonart@gmail.com>
2026-09-04 17:00:17 -0300

    week 01 solved

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,16 @@
-import { setLocalStorage } from './utils.mjs';
+import { setLocalStorage, getLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+  let itensCart = getLocalStorage('so-cart') || []; // get the current cart items from local storage, or initialize an empty array if none exist
+  if (!itensCart.some((item) => item.Id === product.Id)) {
+    // prevent adding duplicate items to the cart by checking if the product already exists in the cart
+    itensCart.push(product);
+  }
+
+  setLocalStorage('so-cart', itensCart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {

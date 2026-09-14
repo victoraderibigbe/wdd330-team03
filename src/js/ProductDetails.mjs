f2c5ba31a91b73fcf6f23d3d3d5e83ee4f9503ed
Artur Calderon <artur.calderonart@gmail.com>
2026-09-14 10:50:35 -0300

    team activity W02 done

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,42 @@
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
+
+export default class ProductDetails {
+    constructor(productId, dataSource) {
+        this.productId = productId;
+        this.dataSource = dataSource;
+        this.product = {}
+    }
+
+
+    async init(){
+        this.product = await this.dataSource.findProductById(this.productId);
+        this.renderProductDetails(this.product);
+
+        document.getElementById('addToCart').addEventListener('click', this.addProductToCart.bind(this));
+    }
+
+    addProductToCart() {
+        let itensCart = getLocalStorage('so-cart') || []; // get the current cart items from local storage, or initialize an empty array if none exist
+        if (!itensCart.some((item) => item.Id === this.product.Id)) {
+        // prevent adding duplicate items to the cart by checking if the product already exists in the cart
+        itensCart.push(this.product);
+        }
+        setLocalStorage('so-cart', itensCart);
+    }
+
+    renderProductDetails(product) {
+        document.querySelector('h2').textContent = product.Brand.Name;
+        document.querySelector('h3').textContent = product.NameWithoutBrand;
+
+        const productImage = document.getElementById('productImage');
+        productImage.src = product.Image;
+        productImage.alt = product.NameWithoutBrand;
+
+        document.getElementById('productPrice').textContent = product.FinalPrice;
+        document.getElementById('productColor').textContent = product.Colors[0].ColorName;
+        document.getElementById('productDesc').innerHTML = product.DescriptionHtmlSimple;
+
+        document.getElementById('addToCart').dataset.id = product.Id;
+        }
+
+}

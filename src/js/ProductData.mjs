9cf47d33948edf0abb19f3845256dccc11883bf1
laurelcerrato <lcerratoramirez@byupathway.edu>
2026-09-22 17:45:13 -0600

    hardcoded variable to publish on render

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,4 +1,4 @@
-const baseURL = import.meta.env.VITE_SERVER_URL
+const baseURL = import.meta.env.VITE_SERVER_URL || 'https://wdd330-backend-osp8.onrender.com/';
 function convertToJson(res) {
   if (res.ok) {
     return res.json();

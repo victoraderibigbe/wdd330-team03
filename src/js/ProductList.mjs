a5162590b99c96c7b376f7f9d2d1f60d0f79496c
laurelcerrato <lcerratoramirez@byupathway.edu>
2026-09-22 15:45:18 -0600

    fixed product listing product link

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -3,7 +3,7 @@ import { renderListWithTemplate } from './utils.mjs';
 function productCardTemplate(product) {
   // eslint-disable-next-line no-console
   return `<li class="product-card">
-    <a href="product_pages/?product=${product.Id}">
+    <a href="/product_pages/?product=${product.Id}">
       <img src="${product.Images.PrimaryMedium}" alt="${product.NameWithoutBrand}" />
       <h3 class="card__brand">${product.Brand.Name}</h3>
       <h2 class="card__name">${product.NameWithoutBrand}</h2>
